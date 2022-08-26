09a71046e1095c855c04eecaa85a44dc140e8215
764PEII <109246495+764PEII@users.noreply.github.com>
2022-08-26 20:10:58 +0300

    Update les7_3.py

--- les7_3.py
+++ les7_3.py
@@ -2,13 +2,13 @@ class Cell:
     def __init__(self,k):
         self.k = k
     def __add__(self, other):
-        return self.k + other.k
+        return Cell(self.k + other.k)
     def __sub__(self, other):
-        return self.k - other.k
+        return Cell(self.k - other.k)
     def __mul__(self, other):
-        return self.k*other.k
+        return Cell(self.k*other.k)
     def __truediv__(self, other):
-        return f'{self.k/other.k:.0f}'
+        return Cell(f'{(self.k/other.k):.0f} ')
     def make_order(self):
         if (self.k)%5 == 0:
             for u in range(int(self.k/5)):
@@ -24,11 +24,7 @@ class Cell:
                 for t in range(self.k%5):
                     self.list.append('*')
 
-            print(''.join(self.list))
-
-
-
-
+            return Cell(''.join(self.list))
 cell_0 = Cell(14)
 cell_1 = Cell(15)
 cell_0.make_order()
